239ba3b9ab55e0d4e3b170e05ece211559386a69
Manan Jethwani <2019282@iiitdmj.ac.in>
2021-09-09 19:24:15 +0530

    updated url post logic and added frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,96 @@
-import './App.css';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import { Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import axios from 'axios';
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const postUrl = async (url) => {
+    console.log(url);
+    const result = await axios.post('http://localhost:3001/url', {
+      'url': url
+    });
+    console.log(result);
+  }
+
+  const submitUrl = (event) => 
+  {
+    const form = event.currentTarget;
+    const url = form.querySelector('div>input').value;
+    postUrl(url);
+    event.preventDefault();
+    handleClose();
+  }
+
   return (
-    <div className="App">
-       
-    </div>
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Amet</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#">Home</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="https://github.com/MananJethwani/amazon-ekart-tracker">
+                Github
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div
+        style={{
+          backgroundImage:
+            "linear-gradient(rgba(255,255,255,.75), rgba(255,255,255,.75)), url(/hero-bg.jpg)",
+        }}
+        className="container-fluid main fixed-top"
+      >
+        <div className="row rw">
+          <div className="offset-md-2 col-md-8 offset-1 col-10">
+            <h1>Welcome to AMET!!</h1>
+            <h3>
+              The one stop for traking and reviewing amazon products in best
+              way!
+            </h3>
+          </div>
+        </div>
+      </div>
+      <Button className="button" variant="primary" onClick={handleShow}>
+        +
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form id="url" onSubmit={submitUrl}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Product Url</Form.Label>
+              <Form.Control type="text" placeholder="Ener Url" />
+              <Form.Text className="text-muted">
+                Product might take a minute to be ready.
+              </Form.Text>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" form="url" type="submit">
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
--- src/routes/url.js
+++ src/routes/url.js
@@ -2,10 +2,39 @@ const { Router } = require("express");
 const router = Router();
 const { Url } = require('../models/url');
 const { Email } = require("../models/email");
-const {ini, urlNameExtractor, urlValueExtractor, close} = require("../util/extractor");
+const axios = require("axios");
+const { parse } = require('node-html-parser');
+
+const urlValueExtractor = async (url) => {
+    const { data } = await  axios.get(url, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    const dom = parse(data);
+    const price = parseInt((dom.querySelector('.priceBlockBuyingPriceString') || dom.querySelector('.priceBlockDealPriceString')).innerHTML.slice(1).split('.')[0].split(',').join(''));
+    return price;
+}
+
+const urlNameExtractor = async (url) => {
+    const { data } = await  axios.get(url, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    const dom = parse(data);
+    let name = dom.querySelector('.product-title-word-break').innerHTML;
+    name = name.trim();
+    return name;
+}
 
 router.post('/', async (req, res) => {
-    await ini();
     const entry = await Url.findOne({url: req.body.url});
     const name = await urlNameExtractor(req.body.url);
     if(entry) { return res.status(400).send('URL already exsists'); }
@@ -30,7 +59,6 @@ router.post('/', async (req, res) => {
 
     await url.save();
     await emails.save();
-    await close();
     return res.status(200).send(await Url.find({}));
 });
 
